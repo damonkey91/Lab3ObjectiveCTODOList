bd990f36e3815fb5241c823c89941717f33b496a
mrx <tobjo057@student.liu.se>
2018-01-29 18:47:21 +0100

    first storyboard layout, added classes to views, created model class

---
+++ Labb3ObjectiveCTODOList/TodoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodoListTableViewController.h
+//  Labb3ObjectiveCTODOList
+//
+//  Created by lösen är 0000 on 2018-01-29.
+//  Copyright © 2018 lösen är 0000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoListTableViewController : UITableViewController
+
+@end
---
+++ Labb3ObjectiveCTODOList/tableModule.h
@@ -0,0 +1,13 @@
+//
+//  tableModule.h
+//  Labb3ObjectiveCTODOList
+//
+//  Created by lösen är 0000 on 2018-01-29.
+//  Copyright © 2018 lösen är 0000. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tableModule : NSObject
+
+@end
